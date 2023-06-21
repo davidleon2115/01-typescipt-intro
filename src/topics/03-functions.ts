5dbadb0be345f629a57b8f88cfef193ff131f70e
davidleon2115 <leon95024@gmail.com>
2023-06-21 09:18:27 -0500

    functions with interfaces

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -11,12 +11,35 @@ function multiply( firstNumber: number, secondNumber?: number, base: number = 2 
     return firstNumber * base
 }
 
-const result: number = addNumbers( 1, 2 )
-const resultArrow: string = addNumbersArrow( 10, 20 )
-const multiplyResult: number = multiply(5,6)
+// const result: number = addNumbers( 1, 2 )
+// const resultArrow: string = addNumbersArrow( 10, 20 )
+// const multiplyResult: number = multiply(5,6)
+// console.log({ result })
+// console.log({ resultArrow })
+// console.log({ multiplyResult })
 
-console.log({ result })
-console.log({ resultArrow })
-console.log({ multiplyResult })
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = ( character: Character, amount: number ) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${ this.hp }`)
+    }
+}
+
+healCharacter( strider, 10 )
+healCharacter( strider, 20 )
+
+strider.showHp();
 
 export {}
+
