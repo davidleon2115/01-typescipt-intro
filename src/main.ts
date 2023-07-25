3b8a39db96ef0e713041cf77fe2e0d31c9727e39
David Leon <leon95024@gmail.com>
2023-07-24 23:59:04 -0500

    Homework

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 //import './topics/01-basic-types'
 //import './topics/02-object-interface'
-import './topics/03-functions'
+// import './topics/03-functions'
+import './topics/04-homework-types'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola Mundo`
