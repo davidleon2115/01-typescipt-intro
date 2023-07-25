06fde60e8766f90a072a43fbc23a7138e9fe1d3e
David Leon <leon95024@gmail.com>
2023-07-25 00:43:20 -0500

    05 in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 //import './topics/01-basic-types'
 //import './topics/02-object-interface'
 // import './topics/03-functions'
-import './topics/04-homework-types'
+// import './topics/04-homework-types'
+import './topics/05-basic-destructuring'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola Mundo`
